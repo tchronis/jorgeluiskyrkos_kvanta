55e2a69df0f021f471a26f0ddde0745f9194046a
Ioannis Michelakos <imichelakos@gmail.com>
2019-06-18 13:47:18 +0300

    typos corrected, messages added

--- src/main/webapp/app/home/home-modal.component.ts
+++ src/main/webapp/app/home/home-modal.component.ts
@@ -3,44 +3,28 @@ import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 const ORGANIZATIONS = [
     {
-        name: 'Organization  1',
+        name: 'Χαμόγελο του παιδιού',
         iban: 'GR8702544832402111065962522'
     },
     {
-        name: 'Organization  2',
+        name: 'Κιβωτός του κόσμου',
         iban: 'GR6190765661979613338457127'
     },
     {
-        name: 'Organization  3',
+        name: 'ΑΝΙΜΑ',
         iban: 'GR3936443266453058385938223'
     },
     {
-        name: 'Organization  4',
+        name: 'ΑΡΣΙΣ',
         iban: 'GR3357269008821742111161470'
     },
     {
-        name: 'Organization  5',
+        name: 'ΚΑΝΕ ΜΙΑ ΕΥΧΗ',
         iban: 'GR8311635939078022937700463'
     },
     {
-        name: 'Organization  6',
+        name: 'ΕΛΕΠΑΠ',
         iban: 'GR1748922613603592723901530'
-    },
-    {
-        name: 'Organization  7',
-        iban: 'GR0401925451733247916219787'
-    },
-    {
-        name: 'Organization  8',
-        iban: 'GR7918414673810867305306415'
-    },
-    {
-        name: 'Organization  9',
-        iban: 'GR0607879805723120460356271'
-    },
-    {
-        name: 'Organization  10',
-        iban: 'GR4718429019368047629533070'
     }
 ];
 
